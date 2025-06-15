93e9c7cd727966170234382f7219e3f8c4dbc753
Kbot80 <smithkevinb26@gmail.com>
2025-06-15 14:05:29 -0500

    added ability to count words in frankenstein file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+path = ""
+words = []
+def get_book_text(path):
+    count = 0
+    with open(path) as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        for word in words:
+            if word in words:
+                count += 1
+
+    print(f"{count} words found in the document")
+def main():
+   get_book_text("./books/frankenstein.txt")
+
+main()
